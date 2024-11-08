87d38f8024c6c59f40c0da8f7c47b8fbfc2fd14a
textbadal <textbadal@gmail.com>
2024-11-09 01:43:12 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// src/components/Header.js
+import React from 'react';
+
+// src/components/Header.js
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Welcome to My Website</h1>
+        </header>
+    );
+};
+
+export default Header;
